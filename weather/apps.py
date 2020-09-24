d965509079de0cdb38d17d3b6ec91f2596e14e9d
Keith <k.shintaku@gmail.com>
2020-09-24 14:54:15 -0700

    reorganized and added way to change location via url

--- weather/apps.py
+++ weather/apps.py
@@ -1,13 +1,5 @@
 from django.apps import AppConfig
-import os
-import requests
-import json
 
 
 class WeatherConfig(AppConfig):
     name = 'weather'
-
-
-    def get_weather(self, city='Gardena'):
-        weather_url = 'https://api.openweathermap.org/data/2.5/weather?'
-        return requests.get(weather_url + 'q=' + city + '&appid=' + os.environ.get('OPEN_WEATHER_KEY') + '&units=' + 'imperial').json()
--- weather/views.py
+++ weather/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 import requests
-from .apps import WeatherConfig
+import os
+import requests
+import json
 
 # Create your views here.
 loc_obj = {}
 weather_obj = {}
 
-def index(request):
-  call_weather()
+def spec_city(request, city):
+  call_weather(city)
+  # print(city)
   template = loader.get_template('weather/index.html')
   context = {
     'location': loc_obj,
@@ -17,9 +20,20 @@ def index(request):
   }
   return HttpResponse(template.render(context, request))
 
-def call_weather(city='Torrance'):
-  weather_data = WeatherConfig.get_weather(city)
+def index(request):
+  template = loader.get_template('weather/index.html')
+  context = {
+    'location': loc_obj,
+    'weather': weather_obj,
+  }
+  return HttpResponse(template.render(context, request))
+
+def call_weather(city):
+  print(city)
+  weather_data = get_weather(city)
+  # weather_data = WeatherConfig.get_weather(cityy)
   loc_obj['city'] = weather_data['name']
+  print(loc_obj['city'])
   loc_obj['latitude'] = weather_data['coord']['lat']
   loc_obj['longitude'] = weather_data['coord']['lon']
   loc_obj['timezone offset'] = weather_data['timezone']
@@ -29,3 +43,8 @@ def call_weather(city='Torrance'):
   weather_obj['max temperature'] = weather_data['main']['temp_max']
   weather_obj['humidity'] = weather_data['main']['humidity']
   weather_obj['wind speed'] = weather_data['wind']['speed']
+
+def get_weather(city):
+  print(city)
+  weather_url = 'https://api.openweathermap.org/data/2.5/weather?'
+  return requests.get(weather_url + 'q=' + city + '&appid=' + os.environ.get('OPEN_WEATHER_KEY') + '&units=' + 'imperial').json()
