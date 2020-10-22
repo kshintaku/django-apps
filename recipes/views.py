cd68983cd1e7b5a6c3c8c9c55b8aa82b83b92bc0
Keith <k.shintaku@gmail.com>
2020-10-22 14:44:22 -0700

    category pages up and added some formatting

--- recipes/views.py
+++ recipes/views.py
@@ -9,9 +9,15 @@ from .models import Recipe
 def index(request):
 
     temp = """{"ingredients":{"wet ingredients":{"butter (unsalted)":{"amount":1,"unit":"cup"},"egg":{"amount":2,"unit":""},"white sugar":{"amount":0.66,"unit":"cup"},"brown sugar":{"amount":1,"unit":"cup"},"vanilla extract":{"amount":2,"unit":"teaspoon"}},"dry ingredients":{"all-purpose flour":{"amount":3,"unit":"cup"},"baking soda":{"amount":1,"unit":"teaspoon"},"salt":{"amount":1,"unit":"teaspoon"},"matcha powder":{"amount":2,"unit":"tablespoon"},"white chocolate chips":{"amount":1,"unit":"bag"}}},"recipe":{"duration":{"prep time":15,"cook time":15,"rest time":60},"directions":["Mix butter and sugars together until well mixed and smooth.","Add egg and vanilla extract and mix until well combined.","In a separate bowl, combine all dry ingredients. I advise to sift the matcha powder as it clumps easily.","Whisk dry ingredients together to mix everything evenly.","Slowly integrate dry ingredients with wet ingredients","Let rest covered in refrigerator for 60 min to overnight.","Bake at 350F for 15 minutes, rotating tray at the halfway mark."]}}"""
+    category_list = {}
     recipe_list = Recipe.objects.order_by('pub_date')
+    for cat in Recipe.CATEGORIES:
+        my_filter = {}
+        my_filter["category"] = cat[0]
+        category_list[cat[0]] = Recipe.objects.filter(**my_filter)[:1]
     template = loader.get_template("recipes/index.html")
     context = {
+        "category_list": category_list,
         "recipe_list": recipe_list,
     }
     return HttpResponse(template.render(context, request))
@@ -41,8 +47,9 @@ def categoryView(request, category):
     my_filter = {}
     my_filter["category"] = category
     recipe_list = Recipe.objects.filter(**my_filter)
-    template = loader.get_template("recipes/index.html")
+    template = loader.get_template("recipes/category.html")
     context = {
+        "category": category,
         "recipe_list": recipe_list,
     }
     return HttpResponse(template.render(context, request))
