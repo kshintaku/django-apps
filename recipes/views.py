6ed51fdff092a3a03e06dc8e566730de78257e79
Keith <k.shintaku@gmail.com>
2020-10-27 21:52:04 -0700

    merged category template into main index

--- recipes/views.py
+++ recipes/views.py
@@ -7,56 +7,41 @@ from .models import Recipe
 from .forms import SearchForm
 
 
-def index(request):
-
-    temp = """{"ingredients":{"wet ingredients":{"butter (unsalted)":{"amount":1,"unit":"cup"},"egg":{"amount":2,"unit":""},"white sugar":{"amount":0.66,"unit":"cup"},"brown sugar":{"amount":1,"unit":"cup"},"vanilla extract":{"amount":2,"unit":"teaspoon"}},"dry ingredients":{"all-purpose flour":{"amount":3,"unit":"cup"},"baking soda":{"amount":1,"unit":"teaspoon"},"salt":{"amount":1,"unit":"teaspoon"},"matcha powder":{"amount":2,"unit":"tablespoon"},"white chocolate chips":{"amount":1,"unit":"bag"}}},"recipe":{"duration":{"prep time":15,"cook time":15,"rest time":60},"directions":["Mix butter and sugars together until well mixed and smooth.","Add egg and vanilla extract and mix until well combined.","In a separate bowl, combine all dry ingredients. I advise to sift the matcha powder as it clumps easily.","Whisk dry ingredients together to mix everything evenly.","Slowly integrate dry ingredients with wet ingredients","Let rest covered in refrigerator for 60 min to overnight.","Bake at 350F for 15 minutes, rotating tray at the halfway mark."]}}"""
+def index(request, category=None):
     category_list = {}
+    my_filter = {}
     form = SearchForm()
-    recipe_list = Recipe.objects.order_by("pub_date")
-    for cat in Recipe.CATEGORIES:
-        my_filter = {}
-        my_filter["category"] = cat[0]
-        category_list[cat[0]] = Recipe.objects.filter(**my_filter)[:1]
+    category_view = False
+    if category:
+        my_filter["category"] = category
+        category_list[category] = Recipe.objects.filter(**my_filter)
+    elif request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search_term = form.cleaned_data["search_term"]
+            for cat in Recipe.CATEGORIES:
+                my_filter["category"] = cat[0]
+                results = set()
+                recipe_list = Recipe.objects.filter(**my_filter)
+                for recipe in recipe_list:
+                    temp_json = recipe.recipe
+                    ingredients = temp_json["ingredients"]
+                    for list_type in ingredients:
+                        for ingr in ingredients[list_type].keys():
+                            if search_term in ingr:
+                                results.add(recipe)
+                category_list[cat[0]] = list(results)
+            form = SearchForm()
+        else:
+            form = SearchForm()
+    else:
+        for cat in Recipe.CATEGORIES:
+            my_filter["category"] = cat[0]
+            category_list[cat[0]] = Recipe.objects.filter(**my_filter)[:1]
     template = loader.get_template("recipes/index.html")
     context = {
+        "category_view": category_view,
         "category_list": category_list,
-        "recipe_list": recipe_list,
-        "form": form,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def recipeView(request, recipe_id):
-    form = SearchForm()
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    temp_json = recipe.recipe
-    steps = temp_json["recipe"]["directions"]
-    ingredients = temp_json["ingredients"]
-    ing_array = [type for type in temp_json["ingredients"]]
-    name = recipe.name
-    main_url = recipe.img_url
-
-    template = loader.get_template("recipes/recipe.html")
-    context = {
-        "title": name,
-        "main_url": main_url,
-        "ingredients": ingredients,
-        "ing_array": ing_array,
-        "directions": steps,
-        "form": form,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def categoryView(request, category):
-    form = SearchForm()
-    my_filter = {}
-    my_filter["category"] = category
-    recipe_list = Recipe.objects.filter(**my_filter)
-    template = loader.get_template("recipes/category.html")
-    context = {
-        "category": category,
-        "recipe_list": recipe_list,
         "form": form,
     }
     return HttpResponse(template.render(context, request))
@@ -87,3 +72,27 @@ def searchView(request):
         "form": form,
     }
     return HttpResponse(template.render(context, request))
+
+
+def recipeView(request, recipe_id):
+    form = SearchForm()
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    temp_json = recipe.recipe
+    steps = temp_json["recipe"]["directions"]
+    ingredients = temp_json["ingredients"]
+    ing_array = [type for type in temp_json["ingredients"]]
+    name = recipe.name
+    main_url = recipe.img_url
+
+    template = loader.get_template("recipes/recipe.html")
+    context = {
+        "title": name,
+        "main_url": main_url,
+        "ingredients": ingredients,
+        "ing_array": ing_array,
+        "directions": steps,
+        "form": form,
+    }
+    return HttpResponse(template.render(context, request))
+
+
