ce0c3ac2bba31b528a8d43c2b95227881b5ffec2
Keith <k.shintaku@gmail.com>
2020-11-26 23:26:30 -0800

    black formatting

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+
 # Create your views here.
 
 
